0e92d4b26697f01eedc8c0038299aa5d5c4a956e
suekrue <suekrue.cebeci@dci-student.org>
2024-11-27 16:03:28 +0100

    Tailwind

---
+++ frontend/src/App.jsx
@@ -0,0 +1,14 @@
+
+import TaskBoard from "./components/TaskBoard";
+import './index.css';
+// import './App.css';
+
+const App = () => {
+  return (
+    <div className="container mx-auto p-4">
+      <TaskBoard />
+    </div>
+  );
+};
+
+export default App;
